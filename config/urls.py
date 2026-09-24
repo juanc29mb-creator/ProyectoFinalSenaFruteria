3da862f37e111a4f9ccb33718b61e60ecb961d86
Jhon <jhonmor777@gmail.com>
2026-09-24 11:05:41 -0500

    estilos y creacion de panel administrativo

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from principal.views import inicio
+from django.urls import path, include 
+from principal.views import dashboard_view, inicio, logout_view
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),
+    path('', inicio, name='inicio'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='principal/login.html'), name='login'),
+    path('accounts/logout/', logout_view, name='logout'),
   
 ]
